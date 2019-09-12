ec1b7991fddd5834cff7151ed980e640db3eef45
Victor Wardi <victorcaetano@live.com>
2019-09-12 09:13:09 -0300

    Added some methods

--- src/main/java/run/victor/brewery/api/BeerApiApplication.java
+++ src/main/java/run/victor/brewery/api/BeerApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BeerApiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BeerApiApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(BeerApiApplication.class, args);
+  }
 
 }
--- src/main/java/run/victor/brewery/api/controller/BeerController.java
+++ src/main/java/run/victor/brewery/api/controller/BeerController.java
@@ -1,32 +1,59 @@
 package run.victor.brewery.api.controller;
 
-import run.victor.brewery.api.model.BeerDTO;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.UUID;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import run.victor.brewery.api.model.BeerDTO;
+import run.victor.brewery.api.service.BeerService;
+
 /**
- * Created by Victor Wardi - @victorwardi on 9/6/2019
+ * Created by Victor Wardi - @victorwardi on 9/6/2019.
  */
 @RestController
 @RequestMapping("/api/v1/beers")
 public class BeerController {
 
+    private final BeerService beerService;
+
+    public BeerController(BeerService beerService) {
+        this.beerService = beerService;
+    }
+
     @GetMapping("/{beerId}")
-    public ResponseEntity<BeerDTO> getBeerById(@PathVariable("beerId")UUID beerId) {
-        return new ResponseEntity<>(BeerDTO.builder().build(), HttpStatus.OK);
+    public ResponseEntity<BeerDTO> getBeerById(@PathVariable("beerId") UUID beerId) {
+        BeerDTO beerDTO = beerService.getBeerById(beerId);
+        return new ResponseEntity<>(beerDTO, HttpStatus.OK);
     }
 
     @PostMapping()
-    public ResponseEntity<BeerDTO> addBeer(@RequestBody BeerDTO beer) {
-        return new ResponseEntity<>(BeerDTO.builder().build(), HttpStatus.CREATED);
+    public ResponseEntity addBeer(@RequestBody BeerDTO beer) {
+
+        BeerDTO beerDTO = beerService.addBeer(beer);
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.add("Location", "/api/v1/beer/" + beerDTO.getId().toString());
+        return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity<BeerDTO> updateBeer( @PathVariable("beerId")UUID beerId, @RequestBody BeerDTO beer) {
+    public ResponseEntity<BeerDTO> updateBeer(@PathVariable("beerId") UUID beerId, @RequestBody BeerDTO beer) {
         return new ResponseEntity<>(BeerDTO.builder().build(), HttpStatus.NO_CONTENT);
     }
 
+    @DeleteMapping({"/{beerId}"})
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBeer(@PathVariable("beerId") UUID beerId) {
+        beerService.deleteById(beerId);
+    }
 }
