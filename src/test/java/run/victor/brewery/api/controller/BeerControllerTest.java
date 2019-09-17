bd514500111a4ae8675ebc3c019295cb9d53f92b
Victor Wardi <victorcaetano@live.com>
2019-09-17 20:27:27 -0300

    Added Spring Rest docs.

--- src/test/java/run/victor/brewery/api/controller/BeerControllerTest.java
+++ src/test/java/run/victor/brewery/api/controller/BeerControllerTest.java
@@ -19,8 +19,7 @@ import run.victor.brewery.api.service.BeerService;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -94,7 +93,20 @@ class BeerControllerTest {
             post("/api/v1/beers/")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(json))
-            .andExpect(status().isCreated());
+            .andExpect(status().isCreated())
+            .andDo(document("v1/beer",
+                requestFields(
+                    fieldWithPath("id").ignored(),
+                    fieldWithPath("version").ignored(),
+                    fieldWithPath("createdDate").ignored(),
+                    fieldWithPath("lastModifiedDate").ignored(),
+                    fieldWithPath("beerName").description("Name of the beer"),
+                    fieldWithPath("beerStyle").description("Style of Beer"),
+                    fieldWithPath("upc").description("Beer UPC").attributes(),
+                    fieldWithPath("price").description("Beer Price"),
+                    fieldWithPath("quantityOnHand").ignored()
+                )));
+            ;
     }
 
     @Test
